e42a03eabb96d1d840d27ecb41b4e8f1a89b354d
LizzieCamp <elizabeth.camp14@gmail.com>
2023-05-30 16:46:10 +0100

    feat: create components

---
+++ src/components/AboutSection/AboutSection.tsx
@@ -0,0 +1,57 @@
+import { ParallaxLayer } from "@react-spring/parallax";
+import { CSSProperties } from "react";
+import "../../styles/styles";
+
+type AboutItem = {
+  words: string;
+  className: string[];
+  style?: CSSProperties;
+};
+const aboutItems: AboutItem[] = [
+  {
+    words:
+      "Hello👋🏻 I'm Elizabeth, a Frontend Software Engineer, currently working for Elsevier.",
+    className: ["position"],
+    style: { paddingLeft: "200px" },
+  },
+  {
+    words: "I graduated Univeristy in 2019 with a 2.1 in Computer Science.",
+    className: ["position onRight", "right"],
+    style: { width: "100vw", paddingTop: "350px" },
+  },
+  {
+    words:
+      " I have been working professionally as a Software Engineer for just over 3.5 years.",
+    className: ["position"],
+    style: { paddingLeft: "200px", paddingTop: "700px" },
+  },
+  {
+    words: "This will be another super fun fact",
+    className: ["position onRight", "right"],
+    style: { width: "100vw", paddingTop: "1050px" },
+  },
+  {
+    words: "This will be another super fun fact",
+    className: ["position"],
+    style: { paddingLeft: "200px", paddingTop: "1400px" },
+  },
+  {
+    words: "This will be another super fun fact",
+    className: ["position onRight", "right"],
+    style: { width: "100vw", paddingTop: "1750px" },
+  },
+];
+
+const renderAbout = aboutItems.map((item, index) => (
+  <ParallaxLayer
+    offset={2}
+    speed={0.5}
+    style={item.style ? item.style : undefined}
+    className={item.className[0]}
+    key={index}
+  >
+    <p className={`messages + ${item.className[1]}`}>{item.words}</p>
+  </ParallaxLayer>
+));
+
+export const AboutSection = () => <>{renderAbout}</>;
---
+++ src/components/IntroSection/IntroSection.tsx
@@ -0,0 +1,49 @@
+import { ParallaxLayer } from "@react-spring/parallax";
+import { CSSProperties } from "react";
+import "../../styles/styles";
+
+type IntroItem = {
+  word: string;
+  classNames: string[];
+  style?: CSSProperties;
+  speed: number;
+};
+const introItems: IntroItem[] = [
+  {
+    word: "ELIZABETH",
+    classNames: ["elizabethText", "homePageText"],
+    speed: -5,
+  },
+  {
+    word: "CAMP",
+    classNames: ["campText onRight", "homePageText right"],
+    speed: 5,
+    style: { width: "100vw" },
+  },
+  {
+    word: "SOFTWARE",
+    classNames: ["softwareText", "homePageText"],
+    speed: -4,
+  },
+  {
+    word: "ENGINEER",
+    classNames: ["engineerText onRight", "homePageText right"],
+    speed: 4,
+    style: { width: "100vw" },
+  },
+];
+
+const renderWords = introItems.map((item, index) => (
+  <ParallaxLayer
+    horizontal={true}
+    offset={0}
+    speed={item.speed}
+    className={item.classNames[0]}
+    style={item.style ? item.style : undefined}
+    key={index}
+  >
+    <p className={item.classNames[1]}>{item.word}</p>
+  </ParallaxLayer>
+));
+
+export const IntroSection = () => <>{renderWords}</>;
