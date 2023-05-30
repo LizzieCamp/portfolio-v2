e956b12f25eebc4708522209637da0859741186e
LizzieCamp <elizabeth.camp14@gmail.com>
2023-05-30 16:45:40 +0100

    refactor: tidy this up

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import { Parallax } from "@react-spring/parallax";
-import React from "react";
 import { About } from "./components/About/About";
-import { Test } from "./components/Test/Test";
-import { ParticlesBackground } from "./Particles";
-// import './App.css';
 import "./styles/styles";
 
 const App = () => {
   return (
     <div>
-        <About />
-      
+      <About />
     </div>
   );
 };
